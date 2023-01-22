cd91ab9554b335c650a63184bc8e0d043740f7a6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-01-22 16:05:24 +0530

    Create 2544.py

---
+++ 2501-2600/2544.py
@@ -0,0 +1,9 @@
+class Solution:
+    def alternateDigitSum(self, n: int) -> int:
+        s=str(n)
+        ans=0
+        for i in range(0,len(s),2):
+            ans+=int(s[i])
+        for i in range(1,len(s),2):
+            ans-=int(s[i])
+        return ans
