d4ea6ad5dcb6c8154cb85674dc80721c3ce4441e
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-14 10:26:07 +0530

    Create 1534.py

---
+++ 1501-2000/1534.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        good_triplets = 0
+        length = len(arr)
+
+        for i in range(length):  # i is the first index
+            for j in range(i + 1, length):  # j must be after i
+                if abs(arr[i] - arr[j]) <= a:  # first condition check
+                    for k in range(j + 1, length):  # k must be after j
+                        # Check all three conditions
+                        if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                            good_triplets += 1
+                            # 🥋 Rock Lee: "Another success from persistence!" 🥳
+        
+        return good_triplets
